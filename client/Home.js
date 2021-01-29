c7849ca4281aa336cc529c4e837f3955d52180ba
jaznyc <jaznycdev@gmail.com>
2021-01-28 20:33:56 -0500

    find similar candidates

--- client/Home.js
+++ client/Home.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return <div>This is the Home Page</div>;
-};
+class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: '',
+      candidate: {},
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    const val = evt.target.value;
+    this.setState({ id: +val });
+  }
+
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    evt.persist();
+    const id = this.state.id;
+    try {
+      const { data } = await axios.get(`/api/candidates/${id}`);
+      this.setState({ candidate: data });
+    } catch (err) {
+      console.log('error submitting id:', err);
+    }
+  }
+
+  render() {
+    const candidate = this.state.candidate;
+    console.log(candidate);
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="id"> Enter Your Candidate Id Here: </label>
+          <input name="id" type="text" onChange={this.handleChange} />
+          <button>Submit</button>
+        </form>
+        <div> {candidate ? <h1>{candidate.title}</h1> : <div></div>} </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
--- server/api/candidates.js
+++ server/api/candidates.js
@@ -1,16 +1,24 @@
 
 const { Candidate } = require('../db');
 const { Company } = require('../db');
+const Sequelize = require('sequelize')
 const router = require('express').Router();
 module.exports = router;
 
 router.get('/:candidateId', async (req, res, next) => {
   try {
+    console.log("hit")
     const singleCandidate = await Candidate.findOne({
       where: {
         candidate_id: req.params.candidateId,
       },
+      include: [
+      {model: Company,
+      on: {
+        col1: Sequelize.where(Sequelize.col("candidate.company_id"), "=", Sequelize.col("company.company_id"))
+      }}]
     });
+    await similarCandidiates(singleCandidate)
     res.json(singleCandidate);
   } catch (err) {
     next(err);
@@ -18,3 +26,31 @@ router.get('/:candidateId', async (req, res, next) => {
 });
 
 
+const similarCandidiates = async (candidateObj)=>{
+  const candidates = await Candidate.findAll({
+    where: {
+      title: candidateObj.title,
+    },
+    include: [
+      {model: Company,
+      on: {
+        col1: Sequelize.where(Sequelize.col("candidate.company_id"), "=", Sequelize.col("company.company_id"))
+      }}]
+
+  });
+  const simpleCandidates = candidates.map((elem)=>{
+    return({
+    id: elem.candidate_id,
+    coding: elem.coding_score,
+    communication: elem.communication_score,
+    title: elem.title,
+    companyId: elem.company_id,
+    fractal: elem.company.fractal_index
+  })
+  }).filter((obj)=>{
+    return(
+      Math.abs(candidateObj.company.fractal_index - obj.fractal) < 0.15
+    )
+  })
+  console.log('candidates', simpleCandidates)
+}
